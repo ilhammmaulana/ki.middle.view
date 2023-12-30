70f700b2e421edd71a032e966ca8dec929a35bb3
ilhammmaulana <ilhammmaulana688@gmail.com>
2023-12-30 09:41:58 +0700

    remove: some file

--- 8/index.blade.php
+++ 8/index.blade.php
@@ -12,9 +12,6 @@
 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
             integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-        <!-- Bootstrap core CSS -->
-
-        <!-- Custom styles for this template -->
         <link href="album.css" rel="stylesheet">
     </head>
 
@@ -111,19 +108,12 @@
                 <p>Copyright 2024 &copy; {Nama kamu}</p>
             </div>
         </footer>
-
-        <!-- Bootstrap core JavaScript
-    ================================================== -->
-        <!-- Placed at the end of the document so the pages load faster -->
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-            integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
-        </script>
-        <script>
-            window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')
-        </script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
         </script>
     </body>
 
 </html>
+
+
+{{-- Thanks to : https://github.com/ilhammmaulana/ki.resource.middle/ --}}
